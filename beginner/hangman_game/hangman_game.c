59a627f504ed6ec005bcae2505e2f5a56539817a
alemoles <alemolerio@gmail.com>
2024-10-27 20:19:36 -0300

    set up hangman game file

---
+++ beginner/hangman_game/hangman_game.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define MAX_WORD_LENGTH 50
+#define MAX_TRIES 6
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
