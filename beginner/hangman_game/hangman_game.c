6111cabe0742dddf3d3bc771b93c74ca497b3fb4
alemoles <alemolerio@gmail.com>
2024-10-27 20:53:20 -0300

    create basic structure for game

--- beginner/hangman_game/hangman_game.c
+++ beginner/hangman_game/hangman_game.c
@@ -8,6 +8,16 @@
 #define MAX_WORD_LENGTH 50
 #define MAX_TRIES 6
 
+struct WordHint {
+    char word[MAX_WORD_LENGTH];
+    char hint[MAX_WORD_LENGTH];
+};
+ 
+void displayWord(const char word[], const bool guessed[]);
+ 
+// Function to draw the hangman
+void drawHangman(int tries);
+
 int main(int argc, char const *argv[])
 {
 
