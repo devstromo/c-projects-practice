02f079c8935762cd2c12a5777d4d79d7faed64a3
alemoles <alemolerio@gmail.com>
2025-03-16 16:25:16 -0300

    Add basic calculator implementation

--- beginner/basic_calculator/basic_calculator.c
+++ beginner/basic_calculator/basic_calculator.c
@@ -1,33 +1,40 @@
 #include <stdio.h>
 
-int main()
+// Function to perform calculations
+double calculate(int a, int b, int op)
 {
-    int a, b, op;
-    printf("Enter first integer number\n");
-    scanf("%d", &a);
-    printf("Enter second integer number\n");
-    scanf("%d", &b);
-    printf("Enter operation number\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n");
-    scanf("%d", &op);
     switch (op)
     {
     case 1:
-        printf("\n\t\t\t\tThe sum of %d and %d is %d\n", a, b, a + b);
-        break;
+        return a + b;
     case 2:
-        printf("\n\t\t\t\tThe difference of %d and %d is %d\n", a, b, a - b);
-        break;
+        return a - b;
     case 3:
-        printf("\n\t\t\t\tThe product of %d and %d is %d\n", a, b, a * b);
-        break;
+        return a * b;
     case 4:
-        if (b == 0)
-            printf("\n\t\t\t\tDivision by zero is not allowed\n");
-        else
-            printf("\n\t\t\t\tThe division of %d and %d is %.2f\n", a, b, a / (double)b);
-        break;
+        return (b != 0) ? (double)a / b : 0;
     default:
-        printf("\n\t\t\t\tInvalid operation\n");
+        return -999999;
+    }
+}
+
+int main()
+{
+    int a, b, op;
+    printf("Enter first integer number: ");
+    scanf("%d", &a);
+    printf("Enter second integer number: ");
+    scanf("%d", &b);
+    printf("Enter operation number\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n");
+    scanf("%d", &op);
+
+    if (op == 4 && b == 0)
+    {
+        printf("\n\t\t\t\tDivision by zero is not allowed\n");
+    }
+    else
+    {
+        printf("\n\t\t\t\tResult: %.2f\n", calculate(a, b, op));
     }
 
     return 0;
