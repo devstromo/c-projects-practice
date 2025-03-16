9c5bd5c58c1afd408a0c0f6ce37d988841355efd
alemoles <alemolerio@gmail.com>
2025-03-16 17:15:34 -0300

    Add basic calculator implementation

--- beginner/basic_calculator/basic_calculator.c
+++ beginner/basic_calculator/basic_calculator.c
@@ -17,25 +17,3 @@ double calculate(int a, int b, int op)
         return -999999;
     }
 }
-
-// int main()
-// {
-//     int a, b, op;
-//     printf("Enter first integer number: ");
-//     scanf("%d", &a);
-//     printf("Enter second integer number: ");
-//     scanf("%d", &b);
-//     printf("Enter operation number\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n");
-//     scanf("%d", &op);
-
-//     if (op == 4 && b == 0)
-//     {
-//         printf("\n\t\t\t\tDivision by zero is not allowed\n");
-//     }
-//     else
-//     {
-//         printf("\n\t\t\t\tResult: %.2f\n", calculate(a, b, op));
-//     }
-
-//     return 0;
-// }
