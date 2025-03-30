1342552d138e8f92f53be9ce99b032fb5db6b571
alemoles <alemolerio@gmail.com>
2025-03-29 23:17:59 -0300

    add game modes

--- beginner/guessing_game/guessing_game.c
+++ beginner/guessing_game/guessing_game.c
@@ -7,11 +7,39 @@ int main()
     srand(time(NULL));
     int r = (rand() % 100) + 1;
     printf("Welcom to random number guessing game\n");
+    printf("First, select the mode option:\n\t1. Chill(No limit)\n\t2. Normal(10 attempts)\n\t3. Hard(5 attempts)\n");
+    int mode = 0;
+    scanf("%d", &mode);
+    int attemptsLimit = 0;
+    if (mode == 1)
+    {
+        printf("You have selected Chill mode\n");
+    }
+    else if (mode == 2)
+    {
+        printf("You have selected Normal mode\n");
+        attemptsLimit = 10;
+    }
+    else if (mode == 3)
+    {
+        printf("You have selected Hard mode\n");
+        attemptsLimit = 5;
+    }
+    else
+    {
+        printf("Invalid mode selected. Exiting...\n");
+        return 1;
+    }
     printf("Guess a number between 1 and 100\n");
     int guess = 0;
+
     int attempts = 0;
-    while (guess != r)
+    while (guess != r && (attemptsLimit == 0 || attempts < attemptsLimit))
     {
+        if (attemptsLimit > 0)
+        {
+            printf("You have %d attempts left\n", attemptsLimit - attempts);
+        }
         printf("Enter your guess: ");
         scanf("%d", &guess);
         attempts++;
@@ -26,6 +54,12 @@ int main()
         else
         {
             printf("Congratulations! You guessed the number in %d attempts.\n", attempts);
+            break;
+        }
+        if (attemptsLimit > 0 && attempts >= attemptsLimit)
+        {
+            printf("Sorry, you've used all your attempts. The number was %d.\n", r);
+            break;
         }
     }
     printf("Would you like to play again? (y/n): ");
