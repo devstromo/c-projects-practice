486c8311d528d9692e45888218d1eb592a226288
alemoles <alemolerio@gmail.com>
2025-05-11 00:45:48 -0300

    Add BankAccount struct definition to main.c

--- intermediate/bank_system/main.c
+++ intermediate/bank_system/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+typedef struct BankAccount
+{
+    int accountNumber;
+    char accountHolder[50];
+    double balance;
+    char accountType[20]; // e.g., Savings, Checking
+    char dateOpened[11]; // Format: YYYY-MM-DD
+    char lastTransactionDate[11]; // Format: YYYY-MM-DD
+} BankAccount;
+
 // MAIN
 
 int main() {
