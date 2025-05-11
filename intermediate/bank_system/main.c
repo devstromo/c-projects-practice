d1eeff7a0645eb07e45e1528a59095db7844795c
alemoles <alemolerio@gmail.com>
2025-05-11 00:46:59 -0300

    Refactor main function to enhance user interaction and menu options for bank account management

--- intermediate/bank_system/main.c
+++ intermediate/bank_system/main.c
@@ -15,7 +15,48 @@ typedef struct BankAccount
 // MAIN
 
 int main() {
-    printf("Hello to bank system!\n");
+    int option;
+
+    printf("Welcome to the Bank Account Management System!\n");
+    printf("1. Add a new account\n");
+    printf("2. View all accounts\n");
+    printf("3. Search for an account\n");
+    printf("4. Update an account\n");
+    printf("5. Delete an account\n");
+    printf("6. Exit\n");
+    printf("Please select an option (1-6): ");
+    scanf("%d", &option);
+    if (option < 1 || option > 6) {
+        printf("Invalid option. Please select a number between 1 and 6.\n");
+        return 1;
+    }
+    switch (option) {
+        case 1:
+            printf("Adding a new account...\n");
+            // Code to add a new account
+            break;
+        case 2:
+            printf("Viewing all accounts...\n");
+            // Code to view all accounts
+            break;
+        case 3:
+            printf("Searching for an account...\n");
+            // Code to search for an account
+            break;
+        case 4:
+            printf("Updating an account...\n");
+            // Code to update an account
+            break;
+        case 5:
+            printf("Deleting an account...\n");
+            // Code to delete an account
+            break;
+        case 6:
+            printf("Exiting the program...\n");
+            break;
+        default:
+            printf("Invalid option. Please select a number between 1 and 6.\n");
+    }
     
     return 0;
 }
