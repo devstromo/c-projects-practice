4907ecaf646b2f4a226a841f27cedf149af8e9da
alemoles <alemolerio@gmail.com>
2025-05-14 20:58:52 -0300

    Add LedgerEntry struct definition to main.c

--- intermediate/bank_system/main.c
+++ intermediate/bank_system/main.c
@@ -11,6 +11,15 @@ typedef struct BankAccount
     char lastTransactionDate[11]; // Format: YYYY-MM-DD
 } BankAccount;
 
+typedef struct LedgerEntry {
+    int transactionId;
+    int fromAccount;
+    int toAccount;
+    double amount;
+    char date[11];     // Format: YYYY-MM-DD
+    char note[100];    // Optional note: e.g., "transfer", "deposit", etc.
+} LedgerEntry;
+
 void initBankAccountSequenceFile()
 {
     FILE *idFile = fopen("bank_account.seq", "w");
