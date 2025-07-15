a054b7c5239abb1ddbe38ef36b5784d907514a0e
alemoles <alemolerio@gmail.com>
2025-07-14 22:58:05 -0300

    Refactor main.c to improve function pointer usage and formatting

--- misc/code_address_as_data/main.c
+++ misc/code_address_as_data/main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-
 void hello()
 {
     printf("Hello, World!\n");
 }
-
 
 int main()
 {
     void *address = (void *)hello;
     printf("Address of hello function: %p\n", address);
     printf("Calling hello function using address...\n");
-    ((void (*)(void))address)();  // Cast address to function pointer and call it
+    ((void (*)(void))address)(); // Cast address to function pointer and call it
     printf("Function call completed.\n");
+    void (*func_ptr)() = hello; // A more portable (and safer) version
+    func_ptr();
     return 0;
 }
