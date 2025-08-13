5098b074791c1faa483185e088446dc2d327afed
alemoles <alemolerio@gmail.com>
2025-08-13 19:30:24 -0300

    Add array pointer demonstration to main function

--- misc/pointer_increase/main.c
+++ misc/pointer_increase/main.c
@@ -5,6 +5,11 @@ int main()
     int x = 5;
     int *p = &x;
     printf("Value of x: %d\n", *p++);
+    int arr[] = {10, 20, 30};
+    int *pa = arr;
 
+    printf("%d\n", *pa++); // prints 10, p now points to arr[1]
+    printf("%d\n", *pa++); // prints 20, p now points to arr[2]
+    printf("%d\n", *pa);
     return 0;
 }
